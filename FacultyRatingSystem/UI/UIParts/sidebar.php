b52a39dee97cb253313000d3a681f32880437dfc
Aly Mama <aly070296@gmail.com>
2025-01-25 14:56:43 +0800

    0.0.2 - Sidebar

--- FacultyRatingSystem/UI/UIParts/sidebar.php
+++ FacultyRatingSystem/UI/UIParts/sidebar.php
@@ -1,4 +1,4 @@
-<aside class="main-sidebar sidebar-dark-primary elevation-4">
+<aside class="main-sidebar sidebar-dark-primary elevation-4" style="background-color: #1c7430!important">
     <a href="index.php" class="brand-link mb-4">
         <img src="FacultyRatingSystem/Skin/dist/img/logo.jpg" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
             style="opacity: .8">
@@ -14,18 +14,69 @@
                     <p>Dashboard</p>
                 </a>
             </li>  
-            <li class="nav-header">RESULTS</li>
+            <li class="nav-header">RATER AND RATEE</li>
             <li class="nav-item">
                 <a href="?elected=true" class="nav-link">
-                    <i class="fas fa-ribbon nav-icon"></i>
-                    <p>Elected Officers</p>
+                    <i class="fas fa-user nav-icon"></i>
+                    <p>Rater</p>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="?elected=true" class="nav-link">
+                    <i class="fas fa-user-tie nav-icon"></i>
+                    <p>Ratee</p>
+                </a>
+            </li>
+            <li class="nav-header">CLASS AND SUBJECT</li>
+            <li class="nav-item">
+                <a href="?user=true" class="nav-link">
+                    <i class="fas fa-book nav-icon"></i>
+                    <p>Subject</p>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="?user=true" class="nav-link">
+                    <i class="fas fa-business-time nav-icon"></i>
+                    <p>A.Y. and Semester</p>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="?user=true" class="nav-link">
+                    <i class="fas fa-chalkboard-teacher nav-icon"></i>
+                    <p>Class</p>
+                </a>
+            </li>
+            <li class="nav-header">QUESTION</li>
+            <li class="nav-item">
+                <a href="?user=true" class="nav-link">
+                    <i class="fas fa-layer-group nav-icon"></i>
+                    <p>Category</p>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="?user=true" class="nav-link">
+                    <i class="fas fa-question nav-icon"></i>
+                    <p>Question</p>
+                </a>
+            </li>
+            <li class="nav-header">ENROLLMENT AND RESULT</li>
+            <li class="nav-item">
+                <a href="?user=true" class="nav-link">
+                    <i class="fas fa-registered nav-icon"></i>
+                    <p>Enrollment</p>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="?user=true" class="nav-link">
+                    <i class="fas fa-clipboard-list nav-icon"></i>
+                    <p>Resut</p>
                 </a>
             </li>
             <li class="nav-header">CONTROL PANEL</li>
             <li class="nav-item">
                 <a href="?user=true" class="nav-link">
                     <i class="fas fa-user-cog nav-icon"></i>
-                    <p>Users</p>
+                    <p>Admin</p>
                 </a>
             </li>
         </ul>
